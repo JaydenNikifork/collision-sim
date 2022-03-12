90c5779e33c4ad74c6ef89779a523b260b724a3c
Jayden Nikifork <jayden.nikifork@gmail.com>
2022-03-12 13:47:16 -0500

    images

--- src/pages.js
+++ src/pages.js
@@ -1,3 +1,13 @@
+import React from 'react';
+
+function importAll(r) {
+    return r.keys().map(r);
+  }
+  
+const QLearnImages = importAll(require.context('./Images/QLearn/', false, /\.(png|jpe?g|svg)$/));
+const FilterImages = importAll(require.context('./Images/Filter/', false, /\.(png|jpe?g|svg)$/));
+const MoneyPrinterImages = importAll(require.context('./Images/MoneyPrinter/', false, /\.(png|jpe?g|svg)$/));
+
 export function About() {
     return(
         <>
@@ -7,7 +17,7 @@ export function About() {
                 </div>
                 <p>My E-Portfolio Site.</p>
             </div>
-            <div className="sub-header about">
+            <div className="about">
                 <h1>About Me</h1>
                 <p>
                     Hello everyone, my name is Jayden Nikifork and I am currently a first year student at McMaster's general engineering program. Despite being an engineering student,
@@ -25,46 +35,87 @@ export function About() {
     );
 }
 
+class ImageShifter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            index: 0
+        }
+    }
+
+    nextImage = () => {
+        this.setState({ index: (this.state.index + 1) % this.props.images.length });
+    }
+
+    prevImage = () => {
+        this.setState({ index: (this.state.index - 1) % this.props.images.length });
+    }
+
+    render() {
+        return(
+            <div className="project-image">
+            <button onClick={ this.nextImage }>&#60;</button>
+                <img src={ this.props.images[this.state.index] } alt="ligma" />
+                <button onClick={ this.nextImage }>&#62;</button>
+            </div>
+        )
+    }
+}
+
 export function Projects() {
+    var QLearnImageIndex = 0;
     return(
         <>
             <div className="main-header projects-title">
-                <h1>My Projects</h1>
+                <h1>Featured Work</h1>
             </div>
-            <div className="sub-header">
+            <div className="project-container">
                 <h1>QLearn</h1>
-                <p>
-                    I'm sure by now all of us students are tired of the annoying homemade homework websites we are forced to use in our classes. With QLearn, assigning and completing
-                    homework is made easy. QLearn is a very simple userfriendly homework Learning Management System in which teachers can post custom announcements and problem sets for their
-                    students. Likewise, students can then submit their answers to the problem sets which can then be viewed by the teacher.
-                    <br /><br />
-                    Developed using: Python, Flask, Jinja2, Bootstrap, MySQL, HTML, and CSS
-                </p>
+                <div className="project-info">
+                    <p>
+                        I'm sure by now all of us students are tired of the annoying homemade homework websites we are forced to use in our classes. With QLearn, assigning and completing
+                        homework is made easy. QLearn is a very simple userfriendly homework Learning Management System in which teachers can post custom announcements and problem sets for their
+                        students. Likewise, students can then submit their answers to the problem sets which can then be viewed by the teacher.
+                        <br /><br />
+                        Developed using: Python, Flask, Jinja2, Bootstrap, MySQL, HTML, and CSS
+                    </p>
+                    <ImageShifter images={QLearnImages} />
+                </div>
             </div>
-            <div className="sub-header">
+            <div className="project-container">
                 <h1>Money Printer</h1>
-                <p>
-                    Everyone wishes they could discover a way to make quick passive income. Well, the console based application, Money Printer, might just be that. Money Printer is
-                    a mock cryptocurrency trading bot that trades on a minutely timeframe. Money Printer acquires data on various coins via Kraken's crypto exchange API, and then calculates
-                    formulas for common trading indicators such as the relative strength index. From there, Money Printer searches for trends and signals amongst the indicators in order
-                    to determine the opportune buy and sell timings of cryptocurrencies. So far before fees, Money Printer actually has the ability to make money especially during
-                    bullish markets, meaning it might be soon time to let it trade on my real portfolio...
-                    <br /><br />
-                    Developed using: Python, and Kraken exchange API
-                </p>
+                <div className="project-info">
+                    <p>
+                        Everyone wishes they could discover a way to make quick passive income. Well, the console based application, Money Printer, might just be that. Money Printer is
+                        a mock cryptocurrency trading bot that trades on a minutely timeframe. Money Printer acquires data on various coins via Kraken's crypto exchange API, and then calculates
+                        formulas for common trading indicators such as the relative strength index. From there, Money Printer searches for trends and signals amongst the indicators in order
+                        to determine the opportune buy and sell timings of cryptocurrencies. So far before fees, Money Printer actually has the ability to make money especially during
+                        bullish markets, meaning it might be soon time to let it trade on my real portfolio...
+                        <br /><br />
+                        Developed using: Python, and Kraken exchange API
+                    </p>
+                    <ImageShifter images={MoneyPrinterImages} />
+                </div>
             </div>
-            <div className="sub-header">
+            <div className="project-container">
                 <h1>Photo Filter</h1>
-                <p>
-                    Well, Photoshop has definitely beaten me to this idea... Anyways, photo filter is a console based application which can apply various imaging filters
-                    to PNG files. The filters include, grayscale, blur, reflection, and edge detection. What the program does, is it takes in a PNG file and filter type via command line
-                    arguement, and then writes a new PNG file with the requested filter applied to the requested image.
-                    <br /><br />
-                    Developed using: C
-                </p>
+                <div className='project-info'>
+                    <p>
+                        Well, Photoshop has definitely beaten me to this idea... Anyways, photo filter is a console based application which can apply various imaging filters
+                        to PNG files. The filters include, grayscale, blur, reflection, and edge detection. What the program does, is it takes in a PNG file and filter type via command line
+                        arguement, and then writes a new PNG file with the requested filter applied to the requested image.
+                        <br /><br />
+                        Developed using: C
+                    </p>
+                    <ImageShifter images={FilterImages} />
+                </div>
             </div>
-            <div className="sub-header">
+            <div className="main-header projects-title">
+                <h1>Additional Work</h1>
+            </div>
+            <div className="project-container">
                 <h1>Spell Checker</h1>
+                <div className='project-info'>
                 <p>
                     Spell checkers are very common amongst many applications, soo why not make my own. Pretty much all this application does is take in 2 text files, one containing
                     a piece of text to be spell checked, and another that contains a dictionary, then a list of words that are spelled wrong are printed in the console. The 
@@ -73,25 +124,35 @@ export function Projects() {
                     <br /><br />
                     Developed using: C
                 </p>
+                </div>
             </div>
-            <div className="sub-header">
-                <h1>Games Recreations</h1>
-                <h2>Battleship</h2>
-                <p>
+            <div className="project-container">
+                <h1>Battleship</h1>
+                <div className='project-info'>
+                    <p>
                     Player vs. AI Battleship game
                     <br />
                     Developed using: Java
-                </p>
-                <h2>Chess</h2>
-                <p>
+                    </p>
+                </div>
+            </div>
+            <div className="project-container">
+                <h1>Chess</h1>
+                <div className='project-info'>
+                    <p>
                     2 Player Chess game
                     <br />
                     Developed using: Java
-                </p>
-                <h2>Sudoku</h2>
-                <p>
+                    </p>
+                </div>
+            </div>
+            <div className="project-container">
+                <h1>Sudoku</h1>
+                <div className='project-info'>
+                    <p>
                     Developed using: Python
-                </p>
+                    </p>
+                </div>
             </div>
         </>
     );
